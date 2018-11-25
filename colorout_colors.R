1e847883af17f771f3b9888229db6effdc4c35ec
williamcioffi <williamcioffi@gmail.com>
2018-11-25 12:48:29 -0500

    sensible defaults for colorout

---
+++ colorout_colors.R
@@ -0,0 +1,3 @@
+library(colorout)
+setOutputColors(normal = 0, negnum = 0, zero = 0, number = 0, date = 0, string = 0, const = 0, false = 0, true = 0, infinite = 0, index = 0, stderror = 28, warn = 208, error = 1)
+
