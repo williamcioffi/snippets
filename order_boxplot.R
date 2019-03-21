6717d1051145a00faac1d450d4e3386c2f01d6f8
williamcioffi <william.cioffi@gmail.com>
2019-03-21 11:31:04 -0400

    order factors

---
+++ order_boxplot.R
@@ -0,0 +1,11 @@
+# how to order a boxplot
+
+# generate some dummy data
+set.seed(1)
+y <- rnorm(100)
+labs <- rep(LETTERS[1:10], each = 10)
+dat <- data.frame(y, labs)
+
+# reverse the order of the levels
+dat$labs <- factor(dat$labs, levels = LETTERS[10:1])
+boxplot(y ~ labs, data = dat)
