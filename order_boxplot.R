9d1161f310b2e4fb2c599f854741bdcb484ba459
williamcioffi <william.cioffi@gmail.com>
2019-03-21 11:33:38 -0400

    update two looks

--- order_boxplot.R
+++ order_boxplot.R
@@ -6,6 +6,11 @@ y <- rnorm(100)
 labs <- rep(LETTERS[1:10], each = 10)
 dat <- data.frame(y, labs)
 
+# take a look
+boxplot(y ~ labs, data = dat)dat
+
 # reverse the order of the levels
 dat$labs <- factor(dat$labs, levels = LETTERS[10:1])
+
+# take another look
 boxplot(y ~ labs, data = dat)
