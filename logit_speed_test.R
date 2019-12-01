e9d7e870fd9eee71391f4f4b6d1a639961d1e31b
williamcioffi <william.cioffi@gmail.com>
2019-12-01 11:56:11 -0500

    miss lib and ignore update
    details:
    - added .DS_Store to ignore because mac os drops those everywhere
    - added library call to logit_speed_test because it needed it
    - plot those benchmarks for easier looking with the eyes

--- logit_speed_test.R
+++ logit_speed_test.R
@@ -1,5 +1,6 @@
 # test which of these is faster
 # this is stupid because i'm never doing a billion logits but i'm curious
+library(microbenchmark)
 
 # logit and invlogit
 logit1 <- function(p) log(p / (1 - p))
@@ -12,7 +13,5 @@ invlogit2 <- function(a) 1 / (1 + exp(-a))
 xx <- seq(0, 1, length = 10000)
 yy <- logit1(xx)
 
-microbenchmark(logit1(xx), logit2(xx), logit3(xx))
-microbenchmark(invlogit1(yy), invlogit2(yy))
-
-hist(logit1(xx) - logit3(xx))
+plot(microbenchmark(logit1(xx), logit2(xx), logit3(xx)))
+plot(microbenchmark(invlogit1(yy), invlogit2(yy)))
